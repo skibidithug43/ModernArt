fa2f4995564a86c542ee85babaa47c11349aaae2
skibidithug43 <103717@atcschool.org>
2024-09-17 11:12:39 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,50 @@
+/*
+Braiden S
+8/26/24
+*/
+
 function setup() {
-  // add your code here. 
+  createCanvas(500,500);
 }
 
 function draw() {
-// add your code here. 
+  background(0,0,0);
   
+  fill(0);
+  rect(200,200,200,200);
+  
+  fill(0);
+  rect(125,125, 150,150);
+  
+  fill(0);
+  ellipse(200,200,50,50);
+}
+
+function draw() {
+  background(155);
+  
+  fill(0);
+  rect(50,100,150,150);
+  
+  fill(100);
+  rect(250,250,50,50);
+  
+  fill(255);
+  rect(100,300,200,200);
+  
+  circle(50, 50, 50);
+  
+    // Style the line.
+  stroke('red');
+  strokeWeight(3);
+
+  line(30, 20, 85, 75);
+  
+  line(0,299,500,299)
+  line(300,500,300,0)
+  line(0,250,500,250)
+  
+  fill(0); //mouse coordinate finder
+
+text("("+mouseX+","+mouseY+")", mouseX, mouseY);
 }
